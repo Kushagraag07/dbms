2aa13b9eddca820758a19d5d901fabad967a11e4
Masktech07 <ka9424@srmist.edu.in>
2024-07-10 12:00:00 +0530

    Improved purchase report structure with validation and table formatting

--- purchase_report.php
+++ purchase_report.php
@@ -1,16 +1,20 @@
 <?php
     include "header.php";
     include "connection.php";
-    $t=0;
-if (isset($_POST['submit'])) 
-{
-    $starttime=$_POST['starttime'];
-    $endtime=$_POST['endtime'];
+    $t = 0;
 
-$sql = "SELECT * FROM purchase where created_at>='$starttime' && created_at<'$endtime'";
-$res = $conn -> query ($sql);
-}
+    if (isset($_POST['submit'])) {
+        $starttime = $_POST['starttime'];
+        $endtime = $_POST['endtime'];
 
+        // Validate that both start and end times are provided
+        if (!empty($starttime) && !empty($endtime)) {
+            $sql = "SELECT * FROM purchase WHERE created_at >= '$starttime' AND created_at <= '$endtime'";
+            $res = $conn->query($sql);
+        } else {
+            echo "Please provide both start and end times.";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -19,57 +23,53 @@ $res = $conn -> query ($sql);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Purchase Report</title>
 </head>
 <body>
 <div class="container">
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-  <label for="starttime">Start (date and time):</label>
-  <input type="datetime-local" id="starttime" name="starttime">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <label for="starttime">Start (date and time):</label>
+        <input type="datetime-local" id="starttime" name="starttime" required>
 
-  <label for="endtime"> End (date and time):</label>
-  <input type="datetime-local" id="endtime" name="endtime">
-  <input type="submit" name="submit">
-</form>
-<button type="button" onclick="window.print();return false;">Pdf Report</button>
-<h5>Purchase Report</h5>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <!--<th scope="col">#</th>-->
-      <th scope="col">Product Name</th>
-      <th scope="col">Unit</th>
-      <th scope="col">Total Unit Price</th>
-    </tr>
-  </thead>
-  <tbody>
- <?php
- if(isset($_POST['submit']))
- {
-          if (mysqli_num_rows($res) > 0) {
-            // output data of each row
-            while($row = mysqli_fetch_assoc($res)) {
-                $t=$t+($row['unit']*$row['unitprice']);
-              ?>
-               <tr>
-                <td><?php echo $row['name'];?></td>
-                <td><?php echo $row['unit'];?></td>
-                <td><?php echo $row['unit']*$row['unitprice'];?></td>
+        <label for="endtime">End (date and time):</label>
+        <input type="datetime-local" id="endtime" name="endtime" required>
 
-                </tr>
-                </form>
-                <?php
-                 }
-        } 
-        else 
-        {
-            echo "0 results";
+        <input type="submit" name="submit" value="Generate Report">
+        <button type="button" onclick="window.print();return false;">PDF Report</button>
+    </form>
+
+    <h5>Purchase Report</h5>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Product Name</th>
+                <th scope="col">Unit</th>
+                <th scope="col">Total Unit Price</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        if (isset($_POST['submit']) && isset($res)) {
+            if (mysqli_num_rows($res) > 0) {
+                // Output data for each row
+                while ($row = mysqli_fetch_assoc($res)) {
+                    $t += ($row['unit'] * $row['unitprice']);
+                    ?>
+                    <tr>
+                        <td><?php echo $row['name']; ?></td>
+                        <td><?php echo $row['unit']; ?></td>
+                        <td><?php echo $row['unit'] * $row['unitprice']; ?></td>
+                    </tr>
+                    <?php
+                }
+            } else {
+                echo "<tr><td colspan='3'>No results found for the specified time period.</td></tr>";
+            }
         }
-    }
         ?>
-    </tbody>
-</table>
-<?php echo "Total= " . $t ." Taka";?>
+        </tbody>
+    </table>
+    <?php if ($t > 0) echo "Total = " . $t . " Taka"; ?>
 </div>
 </body>
 </html>
